a31a912318e9805464937f87d65373a93fbb5842
Júlio Santos <hi@whoisjuliosantos.com>
2022-05-05 16:27:25 +0200

    Update CredentialVerifier.sol

--- CredentialVerifier.sol
+++ CredentialVerifier.sol
@@ -1,20 +1,33 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract CredentialVerifier {
+
     modifier requiresCredential(
         string memory expectedCredential,
-        bytes calldata signature
+        bytes calldata signature,
+        uint validUntil
     ) {
-        require(SignatureChecker.isValidSignatureNow(
-            0x559FfB9C4AB5A552Ed2Ea814A84e74D4CFA21d34,
-            ECDSA.toEthSignedMessageHash(abi.encodePacked(expectedCredential)),
-            signature
-        ));
+        require (
+            block.timestamp < validUntil,
+            "Credential no longer valid"
+        );
 
+        string memory sender = Strings.toHexString(uint256(uint160(msg.sender)), 20);
+        
+        require(
+            SignatureChecker.isValidSignatureNow(
+                0x559FfB9C4AB5A552Ed2Ea814A84e74D4CFA21d34,
+                ECDSA.toEthSignedMessageHash(abi.encodePacked(Strings.toString(validUntil), ";", sender, ";", expectedCredential)),
+                signature
+            ),
+            "Signature doesn't match"
+        );
+        
         _;
     }
 }
